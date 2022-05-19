6fa51baa1febddddfd5e5305af2f4a30168728bb
BeeImp <zero9231@naver.com>
2022-05-19 15:02:12 +0900

    Style: 메세지 내용 정렬

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
   schedule.scheduleJob("0 0 11,15 * * 1-5", () => {
     const nDate = new Date().toLocaleString("ko-KR", {timeZone: "Asia/Seoul"});
     process.env.ALARM_CHANNEL
-    client.channels.cache.get(process.env.ALARM_CHANNEL).send(`${'```'}\n[${nDate}]\n${'```'}🕰째깍이가 울렸어요! 째깍째깍!🕰\n  🥹${'`'}확인 버튼${'`'}을 눌러주세요!🥹`);
+    client.channels.cache.get(process.env.ALARM_CHANNEL).send(`${'```'}\n[${nDate}]\n${'```'}🕰째깍이가 울렸어요! 째깍째깍!🕰\n     🥹${'`'}확인 버튼${'`'}을 눌러주세요!🥹`);
   });
   schedule.scheduleJob("0 5 11,15 * * 1-5", () => {
     client.channels.cache.get(process.env.ALARM_CHANNEL).send(`> 째깍이🕰가 울린지 **5분**이 지났어요!\n> ${'`'}확인 버튼${'`'}을 누르지 않으셨다면 ☑️Click!`);
